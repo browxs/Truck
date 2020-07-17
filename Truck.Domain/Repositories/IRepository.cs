b88814e0a08da455a631223eb98ec1a43964f0aa
Bruno Sousa <bruno_brow7@hotmail.com>
2020-07-17 17:44:28 -0300

    Add project files.

---
+++ Truck.Domain/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truck.Domain.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAsync();
+        Task<T> GetByIdAsync(int id);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Truck.Domain/Repositories/IVeiculoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Truck.Domain.Entities;
+
+namespace Truck.Domain.Repositories
+{
+    public interface IVeiculoRepository : IRepository<Veiculo>
+    {
+        Task<IEnumerable<Veiculo>> GetByMarca(string marca);
+    }
+}
